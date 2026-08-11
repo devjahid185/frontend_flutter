902560463ece7ee717ac1ee6a76734abf0f81fe6
zahid <zahid@MacBookPro.mshome.net>
2026-08-11 18:11:45 +0600

    Point Flutter app to production API

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/foundation.dart';
 
 class AppConfig {
-  static const String _lanBaseUrl = 'https://c744-118-179-184-155.ngrok-free.app/api';
+  static const String _apiBaseUrl = 'https://api.bholavashi.site/api';
   // Web client ID from Google Cloud Console (OAuth 2.0 Client IDs).
   // Needed to get idToken on Android.
   static const String googleWebClientId = '182072072004-33v2h7gofkgg1j2uj3vop8q5npn9mg5c.apps.googleusercontent.com';
 
   static String get apiBaseUrl {
     if (kIsWeb) {
-      return _lanBaseUrl;
+      return _apiBaseUrl;
     }
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return _lanBaseUrl;
+        return _apiBaseUrl;
       default:
-        return _lanBaseUrl;
+        return _apiBaseUrl;
     }
   }
 }
