5081336ff2fc8386258aff694fe9e2da8bdc2a00
zahid <zahid@zahids-MacBook-Pro.local>
2026-08-30 13:08:00 +0600

    Set app default theme to light

--- lib/core/state/theme_manager.dart
+++ lib/core/state/theme_manager.dart
@@ -4,19 +4,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ThemeManager extends ChangeNotifier {
   static const _key = 'theme_mode';
 
-  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode _themeMode = ThemeMode.light;
   ThemeMode get themeMode => _themeMode;
 
   Future<void> initialize() async {
     final prefs = await SharedPreferences.getInstance();
-    final value = prefs.getString(_key);
-    if (value == 'light') {
-      _themeMode = ThemeMode.light;
-    } else if (value == 'dark') {
-      _themeMode = ThemeMode.dark;
-    } else {
-      _themeMode = ThemeMode.system;
-    }
+    await prefs.setString(_key, 'light');
+    _themeMode = ThemeMode.light;
     notifyListeners();
   }
 
